e2179571e2dbfdf62fdca49041f32e13471dd546
Gaurav Kumar <gauravkumar_776@yahoo.co.in>
2023-03-07 17:12:42 +0000

    Unit Test

---
+++ Section15-Testing/Testing_196.py
@@ -0,0 +1,11 @@
+# Each testing code is to test features or development work
+import unittest
+
+
+def do_stuff(num=0):
+    try:
+        if num or num == 0:
+            return int(num) + 5
+        return ('Please enter a number')
+    except ValueError as err:
+        return err
---
+++ Section15-Testing/main.py
@@ -0,0 +1,39 @@
+import unittest
+import Testing_196
+
+
+class TestTesting_196(unittest.TestCase):
+    def test_do_stuff1(self):
+        test_param = 10
+        result = Testing_196.do_stuff(test_param)
+        self.assertEqual(result, 15)
+
+    def test_do_stuff2(self):
+        test_param = 'asdasd'
+        result = Testing_196.do_stuff(test_param)
+        self.assertIsInstance(result, ValueError)
+        # Since result is an instance of the class ValueError hence used this method of unittest.TestCase class
+
+    def test_do_stuff3(self):
+        test_param = None
+        result = Testing_196.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
+        # changed the main.py to add an if statement and a default param
+
+    def test_do_stuff4(self):
+        test_param = ''
+        result = Testing_196.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
+
+    def test_do_stuff5(self):
+        test_param = ' '
+        result = Testing_196.do_stuff(test_param)
+        self.assertIsInstance(result, ValueError)
+
+    def test_do_stuff6(self):
+        test_param = 0
+        result = Testing_196.do_stuff(test_param)
+        self.assertEqual(result, 5)
+
+
+unittest.main()
